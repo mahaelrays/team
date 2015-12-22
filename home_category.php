ed3c3ee7baf9767edabd25ad1b4d84b6030d17b2
sameh-mohamed <sameh.mohamed.kasem@gmail.com>
2015-12-22 12:25:56 +0200

    home_category

---
+++ home_category.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>Task</title> 
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href=css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/validationEngine.jquery.css" type="text/css"/>
+	<link rel="stylesheet" href="css/template.css" type="text/css"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/jquery-ui.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">	
+  </head>
+  <body>
+
+
+
+
+ 
+
+  <!-- category slider -->
+
+<div id="soka-gallery" class="carousel slide  col-lg-12 clear-affix" data-ride="carousel">
+  <div class="carousel-inner  carusal_slide" role="listbox">
+    <div class="item img_gallery active">
+      <img class="img" src="images/slider-1.jpg" alt="...">  
+	    <img class="btn btn-succes" id="offercart" src="images/offer-cart.png" alt="...">
+		 <a href="www.google.com" id="hint1"> <img class="btn btn-succes"  src="images/hint1.png" alt="..."> </a>
+    </div>
+    <div class="item  img_gallery">
+      <img class="img" src="images/slider-2.jpg" alt="...">
+	  <a href="www.google.com" id="offercart"><img class="btn btn-succes"  src="images/offer-cart.png" alt="..."></a>
+	   <a href="www.google.com" id="hint2"> <img class="btn btn-succes"  src="images/hint2.png" alt="..."> </a>
+    </div>
+  </div>
+  <div class="arrows">
+   <a class="left " href="#soka-gallery" role="button" data-slide="prev">
+       <img src="images/arrow_left.jpg" alt="" />
+    <span class="sr-only">Previous</span>
+  </a>
+   <a class="right " href="#soka-gallery" role="button" data-slide="next">
+    <img src="images/arrow_right.jpg" alt="" />
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+  </div>
+  
+    <!-- offers -->
+  <div class="offers">
+   <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+	     <h2>DONT LET FASHION GO TO WASTE</h2>
+		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		 <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+    </div>
+	    <div class="row">
+      <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
+	     <h2>DONT LET FASHION GO TO WASTE</h2>
+		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div id="read_more" class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+    </div>
+	    <div class="row">
+      <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+	     <h2>DONT LET FASHION GO TO WASTE</h2>
+		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a></div>
+	  </div>
+	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+	  <h2>DONT LET FASHION GO TO WASTE</h2>
+	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <h4>soka</h4>
+		 <div class="meta-data"> <span class="glyphicon glyphicon-play"></span><a href="#">Read More...</a> </div>
+	  </div>
+    </div>
+   </div>
+  </div>
+
+ 
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery-1.11.3.min.js"></script> 
+	    <script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+		<script src="js/main.js"></script>
+		<script src="js/bootstrap.js"></script> 
+		<script src="js/jquery-ui.js" ></script>
+		<script src="js/languages/jquery.validationEngine-en.js" type="text/javascript" charset="utf-8"></script>
+	    <script src="js/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
+  </body>
+</html>
