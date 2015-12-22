e070c9f76a93657b78a5f47f2d9a380546dcfc26
sameh-mohamed <sameh.mohamed.kasem@gmail.com>
2015-12-22 14:26:43 +0200

    homecat modified

--- home_category.php
+++ home_category.php
@@ -28,22 +28,22 @@
   <div class="carousel-inner  carusal_slide" role="listbox">
     <div class="item img_gallery active">
       <img class="img" src="images/slider-1.jpg" alt="...">  
-	    <img class="btn btn-succes" id="offercart" src="images/offer-cart.png" alt="...">
-		 <a href="www.google.com" id="hint1"> <img class="btn btn-succes"  src="images/hint1.png" alt="..."> </a>
+	    <img class="btn btn-succes" id="offercart" src="<?php echo get_template_directory_uri(); ?>/images/offer-cart.png" alt="...">
+		 <a href="www.google.com" id="hint1"> <img class="btn btn-succes"  src="<?php echo get_template_directory_uri(); ?>/images/hint1.png" alt="..."> </a>
     </div>
     <div class="item  img_gallery">
       <img class="img" src="images/slider-2.jpg" alt="...">
-	  <a href="www.google.com" id="offercart"><img class="btn btn-succes"  src="images/offer-cart.png" alt="..."></a>
-	   <a href="www.google.com" id="hint2"> <img class="btn btn-succes"  src="images/hint2.png" alt="..."> </a>
+	  <a href="www.google.com" id="offercart"><img class="btn btn-succes"  src="<?php echo get_template_directory_uri(); ?>/images/offer-cart.png" alt="..."></a>
+	   <a href="www.google.com" id="hint2"> <img class="btn btn-succes"  src="<?php echo get_template_directory_uri(); ?>/images/hint2.png" alt="..."> </a>
     </div>
   </div>
   <div class="arrows">
    <a class="left " href="#soka-gallery" role="button" data-slide="prev">
-       <img src="images/arrow_left.jpg" alt="" />
+       <img src="<?php echo get_template_directory_uri(); ?>/images/arrow_left.jpg" alt="" />
     <span class="sr-only">Previous</span>
   </a>
    <a class="right " href="#soka-gallery" role="button" data-slide="next">
-    <img src="images/arrow_right.jpg" alt="" />
+    <img src="<?php echo get_template_directory_uri(); ?>/images/arrow_right.jpg" alt="" />
     <span class="sr-only">Next</span>
   </a>
   </div>
@@ -55,19 +55,19 @@
     <div class="row">
       <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
 	     <h2>DONT LET FASHION GO TO WASTE</h2>
-		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		 <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
 	  </div>
@@ -75,19 +75,19 @@
 	    <div class="row">
       <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
 	     <h2>DONT LET FASHION GO TO WASTE</h2>
-		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div id="read_more" class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
 	  </div>
@@ -95,19 +95,19 @@
 	    <div class="row">
       <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
 	     <h2>DONT LET FASHION GO TO WASTE</h2>
-		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+		 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
 	  </div>
 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
 	  <h2>DONT LET FASHION GO TO WASTE</h2>
-	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+	 <img src="<?php echo get_template_directory_uri(); ?>/images/offer1.jpg" alt="summer offers from factoria" />
 		 <h4>soka</h4>
 		 <div class="meta-data"> <span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a> </div>
 	  </div>
