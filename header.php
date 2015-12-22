920b030f308cdee112f55faa5d99d1477b664b41
Aya AbuElkhair <ayaabuelkhair93@yahoo,com>
2015-12-22 11:47:36 +0200

    header

---
+++ header.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>SOKA</title>
+<?php wp_head();?>
+</head>
+<body>
+<header>
+<nav class=" nav navbars navbar">
+  <div class="navbar-header col-sm-2 soka">
+      SOKA
+  </div>
+<div>
+<ul class="nav nav-tabs colornavul">
+  <li><a href="#home" class="active" style="color:black">Home</a></li>
+  <li><a href="#news">About</a></li>
+  <li><a href="#about">Men</a></li>
+  <li><a href="#about">Wamen</a></li>
+  <li><a href="#about">Girls</a></li>
+  <li><a href="#about">Boys</a></li>
+  <li><a href="#about">Blog</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li id="p1">Login</li>
+  <li><span class="fa-stack">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-search fa-stack-1x"></i>
+  </span></li>
+  <li><span class="fa-stack">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-twitter fa-stack-1x"></i>
+  </span></li>
+  <li><span class="fa-stack">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-vimeo fa-stack-1x"></i>
+  </span></li>
+  <li><span class="fa-stack">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-facebook fa-stack-1x "></i>
+  </span></li>
+</ul> 
+</div>
+</nav>
+</header>
